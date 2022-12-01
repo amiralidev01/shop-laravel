2c2cf7d562f41d157639a342168a7c8a3dbfc487
AmirAli <amiralidev20@gmail.com>
2022-12-01 09:47:53 +0330

    frontend blade implement

---
+++ resources/views/Layout/Frontend/master.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.frontend.header')
+
+@yield('content')
+
+@extends('layout.frontend.footer')
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('create/category', function () {
+    dd(\App\Models\Category::find(1));
+});
